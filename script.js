6051aa26e2acc400ec0dd16d6f4bcf91c5302aa7
sidharth <sp147717@gmail.com>
2026-05-25 19:50:39 +0530

    updated

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+ // Get the logo box and logo elements
  const box = document.getElementById("logo_box");
  const logo = document.getElementById("logo");
-
+// Add a click event listener to change logo box and logo size and position
   document.addEventListener("click", (e) => {
     box.style.left =50 + "px";
     box.style.top = 50 + "px";
